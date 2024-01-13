4237005d67690c6ce5ed75c7e4524f0167abcf13
JordiAllepuz <valencianu@gmail.com>
2024-01-13 23:25:40 +0100

    commit

--- app/src/main/java/com/example/pokedexv2/pokedexappv2/ui/screens/PantallaBusqueda.kt
+++ app/src/main/java/com/example/pokedexv2/pokedexappv2/ui/screens/PantallaBusqueda.kt
@@ -1,12 +1,17 @@
 package com.example.pokedexv2.pokedexappv2.ui.screens
 
+import android.view.RoundedCorner
 import android.widget.ImageButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -16,6 +21,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,6 +35,7 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.rememberImagePainter
@@ -42,23 +50,38 @@ import com.example.pokedexv2.ui.theme.pokemontipografi
 fun PantallaBusqueda(viewModel: PokemonViewModel, navigationController: NavHostController) {
     var text by remember { mutableStateOf("") }
 
-    Image(modifier= Modifier.fillMaxSize(), contentScale = ContentScale.Crop ,painter = painterResource(id = R.drawable.pokeball), contentDescription = "")
+    Image(
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop,
+        painter = painterResource(id = R.drawable.pokeball),
+        contentDescription = ""
+    )
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
         TextField(
             value = text,
             onValueChange = { text = it },
             label = { Text("Buscar Pokemon") },
-            singleLine = true
+            singleLine = true,
+            shape = CircleShape,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color(0xFFDF594F),
+                textColor = Color.Black
+            )
         )
         Button(colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
             onClick = {
-            viewModel.catchPokemon(text)
-            navigationController.navigate(Routes.PokemonFicha.route)
-        }) {
+                viewModel.catchPokemon(text)
+                navigationController.navigate(Routes.PokemonFicha.route)
+            }) {
             Text(
                 "BUSCAR POKEMON",
                 fontFamily = pokemontipografi,
